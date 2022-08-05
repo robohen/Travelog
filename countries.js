268cb2e13a93398639beff591144d1d52e763ee0
robohen <88636103+robohen@users.noreply.github.com>
2022-08-04 22:38:41 -0700

    Add files via upload

---
+++ countries.js
@@ -0,0 +1,29 @@
+var countries = [
+    {
+        id:  "DE",
+        name: "Germany"
+    },
+    {
+        id:  "GR",
+        name: "Greece"
+    },
+    {
+        id:  "IT",
+        name: "Italy"
+    },
+    {
+        id:  "CA",
+        name: "Canada"
+    },
+    {
+        id:  "GB",
+        name: "United Kingdom"
+    },
+    {
+        id:  "US",
+        name: "United States"
+    },    
+    
+];
+
+    
